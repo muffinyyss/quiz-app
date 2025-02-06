c4078706450a7c8ff41410297b15aa590dfc1570
muffinyyss <152370452+muffinyyss@users.noreply.github.com>
2025-02-06 11:17:37 +0700

    quiz-app done ka

---
+++ src/component/Score.js
@@ -0,0 +1,20 @@
+import QuestionsData from "../data/QuestionsData";
+import { DataContext } from "../App";
+import { useContext } from "react";
+
+const Score = () => {
+    const {score, setAppState, setScore} = useContext(DataContext)
+    const restartApp=()=>{
+        setScore(0)
+        setAppState("menu")
+    }
+    return(
+        <div className="score">
+            <h1>สรุปผลคะแนน</h1>
+            <h2>{score} / {QuestionsData.length}</h2>
+            <button onClick={restartApp}>ทำแบบทดสอบอีกครั้ง</button>
+        </div>
+    )
+}
+
+export default Score;
